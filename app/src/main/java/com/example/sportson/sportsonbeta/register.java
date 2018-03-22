8ba95a76f3ef0603d0a42c256002c08fa4495502
rainerr19 <rainerr19@gmail.com>
2018-03-22 10:02:08 -0500

    mejoras
    Se agrego icono y se mejoro el register activity

--- app/src/main/java/com/example/sportson/sportsonbeta/register.java
+++ app/src/main/java/com/example/sportson/sportsonbeta/register.java
@@ -1,7 +1,9 @@
 package com.example.sportson.sportsonbeta;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class register extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle(R.string.reg);// titulo
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//boton de retorno
     }
 }
