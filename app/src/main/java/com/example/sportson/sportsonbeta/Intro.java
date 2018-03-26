32ffbb08d59aa033c6e997b02f50241e0aca1a71
rainerr19 <rainerr19@gmail.com>
2018-03-26 13:14:14 -0500

    restricciones

--- app/src/main/java/com/example/sportson/sportsonbeta/Intro.java
+++ app/src/main/java/com/example/sportson/sportsonbeta/Intro.java
@@ -5,15 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Intro extends AppCompatActivity {
     Button bt_reg;
+    Button btn_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         bt_reg = (Button) findViewById(R.id.btn_reg);
+        btn_in = (Button) findViewById(R.id.btn_inicio);
+
         bt_reg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,6 +26,24 @@ public class Intro extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btn_in.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                EditText use = (EditText) findViewById(R.id.Us);
+                EditText pa = (EditText) findViewById(R.id.PassU);
+                String userdata = use.getText().toString();
 
+                if (userdata.matches("")) {
+                    Toast.makeText(getApplicationContext(), "You did not enter a Email or cell phone number", Toast.LENGTH_SHORT).show();
+                    use.requestFocus();// focus en el editText
+                    return;
+                }
+                userdata = pa.getText().toString();
+                if (userdata.matches("")){
+                    Toast.makeText(getApplicationContext(), "You did not enter a password", Toast.LENGTH_SHORT).show();
+                    pa.requestFocus();// focus en el editText
+                    return;
+                    }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sportson/sportsonbeta/register.java
+++ app/src/main/java/com/example/sportson/sportsonbeta/register.java
@@ -4,9 +4,13 @@ import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
-
+    Button btn_next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +19,44 @@ public class register extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle(R.string.reg);// titulo
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);//boton de retorno
+
+        btn_next = (Button) findViewById(R.id.btn_next) ;
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //si el editText esta vacio
+                EditText NameEdit = (EditText) findViewById(R.id.NameEdit);
+                EditText nmail = (EditText) findViewById(R.id.nmail);
+                EditText Cel = (EditText) findViewById(R.id.Cel);
+                EditText AgeIn = (EditText)findViewById(R.id.AgeIn);
+
+                String Name = NameEdit.getText().toString();
+                if (Name.matches("")) {
+                    Toast.makeText(getApplicationContext(), "You did not enter a username", Toast.LENGTH_SHORT).show();
+                    NameEdit.requestFocus();// focus en el editText
+                    return;
+                }
+                Name = nmail.getText().toString();
+                if (Name.matches("")) {
+                    Toast.makeText(getApplicationContext(), "You did not enter a Email", Toast.LENGTH_SHORT).show();
+                    nmail.requestFocus();
+                    return;
+                }
+                Name = Cel.getText().toString();
+                if (Name.matches("")){
+                    Toast.makeText(getApplicationContext(),"You did not enter a Cell phone number",Toast.LENGTH_SHORT).show();
+                    Cel.requestFocus();
+                    return;
+                }
+                Name = AgeIn.getText().toString();
+                if (Name.matches("")){
+                    Toast.makeText(getApplicationContext(),"You did not enter a Age",Toast.LENGTH_SHORT).show();
+                    AgeIn.requestFocus();
+                    return;
+                }
+
+            }
+        });
+
     }
 }
